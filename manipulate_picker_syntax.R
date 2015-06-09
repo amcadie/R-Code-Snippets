0374dd0c20b82aa5b3c9e65cf939502a5ec69957
Aaron McAdie <aaron.mcadie@gmail.com>
2015-06-09 13:34:40 -0700

    changed plot aesthetics

--- manipulate_picker_syntax.R
+++ manipulate_picker_syntax.R
@@ -1,3 +1,4 @@
 manipulate(ggplot(mtcars, aes_string(x = "disp", y = y.factor)) + 
-                      geom_point(color = "salmon"), 
+                      geom_point(color = "black", size = 7, alpha = 0.5) + 
+                      geom_point(color = "salmon", size = 6, alpha = 0.5), 
                   y.factor = picker("mpg", "hp"))

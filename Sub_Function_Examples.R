722aa71b6d2c8ec8870fe44d8bce583cc8dc4c36
Aaron McAdie <aaron.mcadie@gmail.com>
2015-08-05 10:15:55 -0700

    updated w regex example

--- Sub_Function_Examples.R
+++ Sub_Function_Examples.R
@@ -9,7 +9,12 @@
 Levels: 4 5 8 9 XXX
 > sub(x=test, pattern="XXX", replacement=0)
 [1] "5" "8" "9" "0" "8" "4" "0"
-> test <- sub(x=test, pattern="XXX", replacement=0)
-> test <- as.numeric(test)
-> test
-[1] 5 8 9 0 8 4 0
+
+
+#slicing elements of a character vector and replacing with an element of the slice
+test2 <- c("4.56% excess anode", "4.87% excess anode", "43.897% excess anode")
+
+#pattern regex translates to "group 1 - any character any number of times, group 2 - %,
+#group 3 - any character any number of times". replacement "\\1" replaces with unit 1
+sub(x = test2, pattern = '(.*)(%)(.*)', replacement = '\\1')
+[1] "4.56"   "4.87"   "43.897"
